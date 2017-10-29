5f9c24b080970b8305e720557b2a4e41e9d26b59
alectramell <alectramell@gmail.com>
2017-10-29 01:30:15 -0700

    jumanji

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# jumanji
+![JUMANJI]
+(https://github.com/alectramell/jumanji/raw/master/img/logo.png)
+
+## for linux SHELL ##
---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+clear
+
+git add *.md
+git add *.sh
+git add img
+git add fonts
+git commit -m "jumanji"
+git push origin master
+
+clear
+
+echo "JUMANJI UPDATED!" | pv -qL 10
+
+sleep 0.5
